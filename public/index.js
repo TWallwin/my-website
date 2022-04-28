c9ed823f500d833caaedd3caf4e6730d434f8637
Tom wallwin <TWallwin>
2022-04-28 22:20:50 +0100

    hotfix

--- public/index.js
+++ public/index.js
@@ -96,6 +96,7 @@ function checkCurrentPage(pageIndex) {
 }
 function getDownloads() {
   return fetch("https://my-website-tw.herokuapp.com/api/views").then((res) => {
+    console.log(res);
     return res.data.views;
   });
 }
