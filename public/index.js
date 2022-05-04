d25501dd095798057e830e32b7f8cacaea422b90
Tom wallwin <TWallwin>
2022-05-04 14:09:28 +0100

    counter fixed

--- public/index.js
+++ public/index.js
@@ -1,24 +1,6 @@
 window.onload = () => {
   //fetching view data
   const viewCount = document.querySelector(".view-count");
-  fetch("https://my-website-tw.herokuapp.com/api/views", {
-    method: "POST",
-    mode: "cors",
-  });
-
-  getDownloads().then(async (views) => {
-    function waitms() {
-      return new Promise((resolve) => setTimeout(resolve, 40));
-    }
-
-    for (let i = 0; i <= views.length; i++) {
-      await waitms();
-      if (i < views.length) {
-        i += 1;
-      }
-      viewCount.innerHTML = `${i} Views`;
-    }
-  });
 
   //handling routing
   const pageIndex = {
@@ -39,8 +21,13 @@ window.onload = () => {
     link_spread.classList.add("is-active");
   });
   const loading_symbol = document.querySelector(".lds-dual-ring");
-  loading_symbol.classList.add("loaded");
+  getViews().then((views) => {
+    loading_symbol.classList.add("loaded");
+    observer(views).observe(viewCount);
+  });
+
   checkCurrentPage(pageIndex);
+  //adds event listeners to setup nav links`
   for (let i = 0; i < link_switchers.length; i++) {
     const link_switcher = link_switchers[i];
     const page_id = link_switcher.dataset.link;
@@ -59,57 +46,94 @@ window.onload = () => {
       switchPage(page_id, pageIndex);
     });
   }
-};
+  function switchPage(page_id, pageIndex) {
+    //changes css properties to display correct page and updates current url
+    const current_page = document.querySelector(".pages .is-active");
+    if (current_page) {
+      current_page.classList.remove("is-active");
+    }
 
-function switchPage(page_id, pageIndex) {
-  const current_page = document.querySelector(".pages .is-active");
-  if (current_page) {
-    current_page.classList.remove("is-active");
+    const next_page = document.querySelector(
+      `.pages .page[data-page="${page_id}"]`,
+    );
+    next_page.classList.add("is-active");
+    history.pushState(
+      "data to be passed",
+      "Tom Wallwin",
+      `/${pageIndex[page_id]}`,
+    );
   }
+  function checkCurrentPage(pageIndex) {
+    // uses url to work out what page to show and invokes switchPage to get to show that page
+    const splitUrl = window.location.href.split("/");
 
-  const next_page = document.querySelector(
-    `.pages .page[data-page="${page_id}"]`,
-  );
-  next_page.classList.add("is-active");
-  history.pushState(
-    "data to be passed",
-    "Tom Wallwin",
-    `/${pageIndex[page_id]}`,
-  );
-}
-function checkCurrentPage(pageIndex) {
-  const splitUrl = window.location.href.split("/");
+    let path = splitUrl[splitUrl.length - 1];
 
-  let path = splitUrl[splitUrl.length - 1];
+    if (path[path.length - 1] === "#") {
+      path = path.slice(0, path.length - 1);
+    }
+    if (splitUrl[splitUrl.length - 1] === "") {
+      path = "About";
+    }
+    if (Object.values(pageIndex).indexOf(path) === -1) {
+      const error_page = document.getElementById("error");
+      error_page.classList.add("is-active");
+      return;
+    }
 
-  if (path[path.length - 1] === "#") {
-    path = path.slice(0, path.length - 1);
-  }
-  if (splitUrl[splitUrl.length - 1] === "") {
-    path = "About";
-  }
-  if (Object.values(pageIndex).indexOf(path) === -1) {
-    const error_page = document.getElementById("error");
-    error_page.classList.add("is-active");
-    return;
-  }
-
-  for (let i = 1; i <= Object.keys(pageIndex).length; i++) {
-    if (pageIndex[i] === path) {
-      const link_switchers = document.querySelectorAll("[data-switcher]");
-      const link_switcher = link_switchers[i - 1];
-      link_switcher.classList.add("is-active");
-      switchPage(i, pageIndex);
-      break;
+    for (let i = 1; i <= Object.keys(pageIndex).length; i++) {
+      if (pageIndex[i] === path) {
+        const link_switchers = document.querySelectorAll("[data-switcher]");
+        const link_switcher = link_switchers[i - 1];
+        link_switcher.classList.add("is-active");
+        switchPage(i, pageIndex);
+        break;
+      }
     }
   }
-}
-function getDownloads() {
-  return fetch("https://my-website-tw.herokuapp.com/api/views")
-    .then((res) => {
-      return res.json();
-    })
-    .then((body) => {
-      return body.views;
+  function getViews() {
+    //sends api request to backend to get view array
+    return fetch("https://my-website-tw.herokuapp.com/api/views")
+      .then((res) => {
+        return res.json();
+      })
+      .then((body) => {
+        return body.views;
+      });
+  }
+  function incViews() {
+    //sends api request to backend to add a new view
+    return fetch("https://my-website-tw.herokuapp.com/api/views", {
+      method: "POST",
+      mode: "cors",
     });
-}
+  }
+
+  async function startCounter(views) {
+    //increments view counter html text
+    function waitms() {
+      return new Promise((resolve) => setTimeout(resolve, 40));
+    }
+
+    for (let i = 0; i <= views.length; i++) {
+      await waitms();
+      if (i < views.length) {
+        i += 1;
+      }
+      viewCount.innerHTML = `${i} Views`;
+    }
+    incViews();
+  }
+  const observer = function makeObserver(views) {
+    let startedCounting = false;
+    return new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && !startedCounting) {
+          startCounter(views);
+          startedCounting = true;
+        }
+      },
+      { threshold: [0] },
+    );
+  };
+};
